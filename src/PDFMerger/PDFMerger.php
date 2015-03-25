006a740bb641dcf943187c79b2de4f02f63e97e5
Will Mather <wmather@world-challenge.co.uk>
2015-03-25 16:27:18 +0000

    Change namespace

--- src/PDFMerger/PDFMerger.php
+++ src/PDFMerger/PDFMerger.php
@@ -21,7 +21,7 @@
  * such as form fields, links or page annotations (anything not a part of the page content stream).
  *
  */
-namespace Clegginabox\PDFMerger;
+namespace wmather\PDFMerger;
 
 use Exception;
 use fpdi\FPDI;
